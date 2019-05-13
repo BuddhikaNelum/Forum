6f5b2dad58c6031f49b41c303d02c81836af8969
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-13 08:27:44 +0530

    migrations added

--- database/migrations/2019_05_13_023731_create_questions_table.php
+++ database/migrations/2019_05_13_023731_create_questions_table.php
@@ -15,6 +15,12 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
--- database/migrations/2019_05_13_024114_create_replies_table.php
+++ database/migrations/2019_05_13_024114_create_replies_table.php
@@ -15,6 +15,12 @@ class CreateRepliesTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
