830f47481d41c483ab326e961ac9e3c38193fc33
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-13 14:38:05 +0530

    seeded database

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return App\User::all()->random();
+        }
     ];
 });
