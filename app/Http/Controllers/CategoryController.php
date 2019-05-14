5e839557e9a94a15eb8bf6801efe930520ef2a77
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-14 17:59:14 +0530

    CRUD for Category is implemented

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return Category::latest()->get();
     }
 
     /**
@@ -35,7 +36,12 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      // Category::create($request->all());
+      $category = new Category();
+      $category->name = $request->name;
+      $category->slug = str_slug($request->name);
+      $category->save();
+       return response('created', Response::HTTP_CREATED);
     }
 
     /**
@@ -46,7 +52,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     /**
@@ -69,7 +75,13 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category-> update(
+            [
+                'name'=>$request->name,
+                'slug'=>str_slug($request->name)
+            ]
+        );
+        return response('Updated',Response::HTTP_NO_CONTENT);
     }
 
     /**
@@ -80,6 +92,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response('Deleted', Response::HTTP_NO_CONTENT);
     }
 }
