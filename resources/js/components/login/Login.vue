0b88950d41b2a531a28dc6c2cb996bdf23d8737f
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-15 23:17:57 +0530

    Helper classes and functions added

--- resources/js/components/login/Login.vue
+++ resources/js/components/login/Login.vue
@@ -35,9 +35,7 @@
         },
         methods:{
          login(){
-             axios.post('/api/auth/login', this.form)
-                 .then(res => console.log(res.data))
-                 .catch(error => console.log(error.response.data))
+             User.login(this.form)
          }
         }
     }
