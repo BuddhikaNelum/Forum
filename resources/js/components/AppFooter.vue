0c81d23e40455f61fc2b3af358ac13f6051a5b86
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-15 18:16:45 +0530

    Initial components added

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+        <v-footer class="pa-3">
+            Buddhika Nelum
+            <v-spacer></v-spacer>
+            <div>&copy; {{ new Date().getFullYear() }}</div>
+        </v-footer>
+</template>
+
+<script>
+    export default {
+        name: "AppFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+        <v-toolbar>
+            <!--<v-toolbar-side-icon></v-toolbar-side-icon>-->
+            <v-toolbar-title>Forum</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat>Forum</v-btn>
+                <v-btn flat>Ask Question</v-btn>
+                <v-btn flat>Categoery</v-btn>
+                <v-btn flat>Login</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
